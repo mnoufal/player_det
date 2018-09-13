86ad5e6c83af3f62c8bb77109be27d173b03210d
mnoufal1 <mohammednoufal01@outlook.com>
2018-09-13 10:08:55 -0700

    Third Commit

--- player_detail/player_detail/spiders/player.py
+++ player_detail/player_detail/spiders/player.py
@@ -28,18 +28,44 @@ class MyplayerSpider(scrapy.Spider):
 		
 	def parse_players(self, response):
 		filename = 'page3.html'
-		third_page=response.css("table.playersTable tr td")[0]
+		third_page=response.css("table.playersTable tr td")[5]
 		
 		next_page = third_page.css('a::attr(href)').extract_first()
 		
 		if next_page is not None:
 			next_page = response.urljoin(next_page)
 			yield response.follow(next_page, callback=self.parse_details)
-		
+
 	def parse_details(self, response):
 		filename = 'final_page.html'
+		final_page=response.css("p.ciPlayerinformationtxt")
+		team_list=[]
+		for i in final_page[3].css('span::text').extract():
+			team_list.append(i.strip(','))
+		next_page = response.css('img::attr(src)').extract_first()
+		next_page = response.urljoin(next_page)
+		final_page1=response.css('div.pnl490M')[0]
+		item=[final_page[0].css('span::text').extract_first(),final_page[1].css('span::text').extract_first(),final_page[2].css('span::text').extract_first(),team_list,final_page[4].css('span::text').extract_first(),final_page[5].css('span::text').extract_first(),final_page[6].css('span::text').extract_first(),final_page1.css('h1::text').extract_first(),final_page1.css('h3.PlayersSearchLink b::text').extract_first(),response.url]
+		req = scrapy.Request(next_page, callback=self.write_the_data)
+		req.meta['item']=item
+		yield req
 
-		with open(filename, 'wb') as f:
-            		f.write(response.body)
+	def write_the_data(self, response):
+		page = response.url
+		item = response.meta['item']
+		item.append(page)
+		yield {
+			1: item[7],
+			2: item[8],
+			3: item[0],
+			4: item[1],
+			5: item[2],
+			6: item[3],
+			7: item[4],
+			8: item[5],
+			9: item[6],
+			10: page,
+			11: '',
+			'url': item[9]
 
-
+			}
